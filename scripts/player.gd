808eae818f8c5510a0cf6e92996d32f6b68a93a4
BiGA <berlikl@gmail.com>
2025-10-02 18:44:49 +0200

    Trying to fix turning.

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,3 +1,5 @@
+# TODO during movement disallow another click to set a new target
+
 extends CharacterBody2D
 
 const MOTION_SPEED = 80
@@ -14,7 +16,9 @@ const TAN30DEG = tan(deg_to_rad(30))
 @onready var line_2d = $"../Line2D"
 
 var angle_to_player
+# If we are in the rotation state (turn between tiles) or moving to the next tile.
 var should_rotate = false
+var player_initial_view_angle = 0 # Because the tank looks left
 
 #var target = position
 #var target_pos_clicked
@@ -33,18 +37,15 @@ func _ready():
 	#print('target:', target)
 	#print('tile_map in Player:', tile_map.valid_cells)
 
-# 修改坐标后建立寻路列表
 func _input(event):
 	if event.is_action_pressed("left_click"):
 		var pos_dict = tile_map.position_check(get_global_mouse_position())
-		# 新任务清空
+
 		tile_path.clear()
 		world_path.clear()
 		
-		# 目的地网格坐标和网格中心点坐标
 		var pos_clicked = pos_dict.get('pos_clicked')
 		var local_pos_center_position = pos_dict.get('local_pos_center_position')
-		# 寻路
 
 		if pos_clicked in tile_map.valid_cells and not tile_position == pos_clicked:
 			_animation_player.play("Tank_moves")
@@ -56,46 +57,92 @@ func _input(event):
 			
 			# keep both world and map position arrays in memory
 			tile_path = path_found
-			# 根据路径网格，找回全局坐标
+
 			for node in path_found:
 				var path_position = tile_map.map_to_local(node)
-				#print("路径：", path_position)
+
 				world_path.append(path_position)
 			# draw line
 			#line_2d.points = PackedVector2Array(world_path)
-			# remove first (current) position 移除当前坐标点
+			# remove first (current) position
 			tile_path.remove_at(0)
 			world_path.remove_at(0)
-			# get next position to move to 提出下一个位置的坐标点
+			# get next position to move to
 			tile_position = tile_path.pop_front()
 			target = world_path.pop_front()
 			should_rotate = true
 		else:
 			printerr("need move OR out of area!")
 
+func snap_to_fix_angle(angle):
+	var max_difference = 1
+	var swaps = [30, 90, 120, -150, -90, -30]
+
+	for swapped_angle in swaps:
+		if angle > swapped_angle - max_difference and angle < swapped_angle + max_difference:
+			angle = swapped_angle
+		
+	return angle
+
 func _physics_process(delta):
 	if target:
 		if should_rotate:
-			print(should_rotate, ' ', rotation, ' ', angle_to_player)
+			var delta_sign  = 1
+			#print(
+				#'PRE',
+				#global_position, 
+				#' ', 
+				#(rotation * 180 / 3.14 if rotation is float else 0), 
+				#' ', 
+				#(angle_to_player * 180 / 3.14 if angle_to_player is float else 0)
+			#)
+			#print('GP', global_position, global_position.direction_to(target), )
+			
+			#150 -> -90
+			#-150 -> 150
+			#90 -> -150
+			#-150 -> 90
+			#-90 -> 150
 
-			angle_to_player = global_position.direction_to(target).angle()
-			rotation = move_toward(rotation, angle_to_player, delta)
+			angle_to_player = snap_to_fix_angle(
+				rad_to_deg(
+					global_position.direction_to(target).angle()
+				)
+			)
 
-			if angle_to_player == rotation:
+			# FIXME
+			#if player_initial_view_angle == 150 and angle_to_player == -90:
+				#angle_to_player = 150 + 60
+			
+			rotation_degrees = move_toward(rotation_degrees, angle_to_player, delta_sign * delta * 100)
+
+			print(
+				'POST',
+				should_rotate, 
+				' ', 
+				rotation_degrees, 
+				' ', 
+				angle_to_player
+			)
+			
+			if snap_to_fix_angle(angle_to_player) == snap_to_fix_angle(rotation_degrees):
 				print("rotation stops")
 				should_rotate = false
 		else:
-			#print("moving", Time.get_ticks_msec())
-
+			rotation_degrees = snap_to_fix_angle(rotation_degrees)
+			player_initial_view_angle = rotation_degrees
+			
 			velocity = position.direction_to(target) * speed
 			#look_at(target)
-			if position.distance_to(target) > 5:
+			if position.distance_to(target) > 1:
 				move_and_slide()
 			else:
+				set_position(target)
+
 				# we've reached current destination, get the next one (if any left)
 				if tile_path.size() > 0:
 					tile_position = tile_path.pop_front()
 					target = world_path.pop_front()
-					should_rotate = true
+					should_rotate = true # TODO what if we need to go straight to the next tile.
 				else:
 					_animation_player.stop()
